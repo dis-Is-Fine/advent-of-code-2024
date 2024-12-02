3834fe89c4b99895e6e5134cba9be30c559daad1
dis-Is-Fine <daminakowalczyk2007@gmail.com>
2024-12-02 18:35:00 +0100

    Day 2 | part 2

--- day 2/part 2/solution.py
+++ day 2/part 2/solution.py
@@ -9,20 +9,15 @@ input = open(inputFile, "r")
 
 solution = 0
 
-for line in input:
-    numbers = line.split()
-    for i in range(len(numbers)):
-        numbers[i] = int(numbers[i])
-    
+def checkNumbers(numbers):
     direction = 0
-
+    
     for i in range(len(numbers)):
         if i == len(numbers)-1:
-            solution += 1
-            break
+            return True
 
         diff = numbers[i+1] - numbers[i]
-        
+
         if diff == 0 or abs(diff) > 3:
             break
 
@@ -32,9 +27,30 @@ for line in input:
         if direction < 0:
             if diff > 0:
                 break
-
+            
         if direction > 0:
             if diff < 0:
+                break
+
+    return False
+
+solution = 0
+
+for line in input:
+    numbers = line.split()
+
+    for i in range(len(numbers)):
+        numbers[i] = int(numbers[i])
+
+    safe = checkNumbers(numbers)
+    if safe:
+        solution += 1
+    else:
+        for i in range(len(numbers)):
+            numbersCopy = numbers.copy()
+            numbersCopy.pop(i)
+            if checkNumbers(numbersCopy):
+                solution += 1
                 break
 
 print(f'Solution for the puzzle: {solution}')
